8c43e501dfd6354f00738bf71f00dd57defb7459
Erlend Langseth <3rlendhl@gmail.com>
2021-02-11 19:37:50 +0100

    Initial structure. UI to create a series of names basically...

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
-use iced::{button, Align, Button, Column, Element, Sandbox, Settings, Text};
-use iced_native::{Widget};
-use iced_graphics::{Renderer, Backend};
+use iced::{button, pick_list, text_input};
+use iced::{Align, Button, Column, Element, PickList, Row, Sandbox, Settings, Text, TextInput};
+use iced_graphics::{Backend, Renderer};
+use iced_native::Widget;
+use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
-use serde::{Serialize, Deserialize};
-
 
 use app_dirs::*;
 
-const APP_INFO: AppInfo = AppInfo{name: "Backup", author: "Erlend Langseth"};
+const APP_INFO: AppInfo = AppInfo {
+    name: "Backup",
+    author: "Erlend Langseth",
+};
 
 pub fn main() -> iced::Result {
-
     println!("{:?}", get_app_root(AppDataType::UserConfig, &APP_INFO));
     Ui::run(Settings::default())
 }
 
-#[derive(Default)]
+#[derive(Default, Serialize, Deserialize)]
 pub struct Ui {
     directories: Vec<Directory>,
+    #[serde(skip)]
+    new_button: button::State,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum Message {
+    NewDirectory,
+    Directory(usize, DirectoryMessage),
 }
 
 impl Sandbox for Ui {
@@ -35,35 +41,124 @@ impl Sandbox for Ui {
     }
 
     fn update(&mut self, message: Message) {
-        //
+        match message {
+            Message::NewDirectory => {
+                self.directories.push(Directory::default());
+            }
+            Message::Directory(i, message) => {
+                self.directories[i].update(message);
+            }
+        }
     }
 
     fn view(&mut self) -> Element<Message> {
-        let mut el =Column::new()
+        let directories: Element<_> = self
+            .directories
+            .iter_mut()
+            .enumerate()
+            .fold(Column::new().spacing(20), |column, (i, directory)| {
+                column.push(directory.view().map(move |msg| Message::Directory(i, msg)))
+            })
+            .into();
+        Column::new()
+            .push(directories)
+            .push(
+                Button::new(&mut self.new_button, Text::new("New directory")).on_press(Message::NewDirectory),
+            )
             .padding(20)
-            .align_items(Align::Center);
-        for dir in &mut self.directories {
-            el = el.push(dir.view());
-        }
-        el.into()
+            .align_items(Align::Center)
+            .into()
     }
 }
 
-
-
-
+#[derive(Debug, Clone)]
+pub enum DirectoryMessage {
+    NameChange(String),
+    Dup (usize, DuplicationMessage)
+}
 #[derive(Default, Serialize, Deserialize)]
 pub struct Directory {
+    pub name: String,
     pub path: Option<PathBuf>,
     pub duplication: Vec<Duplication>,
+
+    #[serde(skip)]
+    input_state: text_input::State,
 }
 impl Directory {
-    pub fn view(&mut self) -> Element<Message> {
-        unimplemented!()
+    pub fn update(&mut self, message: DirectoryMessage) {
+        match message {
+            DirectoryMessage::NameChange(name) => self.name = name,
+            DirectoryMessage::Dup(i, msg) => self.duplication[i].update(msg),
+        }
+    }
+    pub fn view(&mut self) -> Element<DirectoryMessage> {
+        Column::new()
+            .padding(20)
+            .align_items(Align::Center)
+            .push(TextInput::new(
+                &mut self.input_state,
+                "Name",
+                &self.name,
+                DirectoryMessage::NameChange,
+            ))
+            .push(
+                self.duplication
+                    .iter_mut()
+                    .enumerate()
+                    .fold(Column::new(), |column, (i, dup)| column.push(dup.view().map(move |msg| DirectoryMessage::Dup (i, msg)))),
+            )
+            .into()
     }
 }
 
-#[derive(Serialize, Deserialize)]
-pub enum Duplication {
-    Disk (PathBuf),
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Duplication {
+    inner: Duplication2,
+
+    #[serde(skip)]
+    pick_list: pick_list::State<DuplicationKind>,
+}
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum Duplication2 {
+    Disk(Option<PathBuf>),
+}
+impl Duplication2 {
+    pub fn new(kind: DuplicationKind) -> Self {
+        match kind {
+            DuplicationKind::Disk => Self::Disk(None),
+        }
+    }
+}
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub enum DuplicationKind {
+    Disk,
+}
+impl std::fmt::Display for DuplicationKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum DuplicationMessage {
+    SelectKind(DuplicationKind),
+}
+
+impl Duplication {
+    pub fn update(&mut self, message: DuplicationMessage) {
+        match message {
+            DuplicationMessage::SelectKind(kind) => self.inner = Duplication2::new(kind),
+        }
+    }
+    pub fn view(&mut self) -> Element<DuplicationMessage> {
+        let options = &[DuplicationKind::Disk][..];
+        Row::new().push(PickList::<_,DuplicationMessage>::new(
+            &mut self.pick_list,
+            options,
+            None,
+            DuplicationMessage::SelectKind,
+        ))
+        .into()
+    }
 }
